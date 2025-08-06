68c6993623486366ee4dfc9b67565e4d9bce90f2
s-olagoke <olagokesikiru06@gmail.com>
2025-08-06 05:30:19 +0100

    chore: cleaned up the modules folder

--- ignition/modules/ERC20.ts
+++ ignition/modules/ERC20.ts
@@ -3,18 +3,12 @@
 
 import { buildModule } from "@nomicfoundation/hardhat-ignition/modules";
 
-const JAN_1ST_2030 = 1893456000;
-const ONE_GWEI: bigint = 1_000_000_000n;
-
-const LockModule = buildModule("LockModule", (m) => {
-  const unlockTime = m.getParameter("unlockTime", JAN_1ST_2030);
-  const lockedAmount = m.getParameter("lockedAmount", ONE_GWEI);
-
-  const lock = m.contract("Lock", [unlockTime], {
-    value: lockedAmount,
+const ERC20Module = buildModule("ERC20Module", (m) => {
+  const erc20 = m.contract("ERC20", ["constrcutorArgs"], {
+    value: args,
   });
 
-  return { lock };
+  return { erc20 };
 });
 
-export default LockModule;
+export default ERC20Module;
